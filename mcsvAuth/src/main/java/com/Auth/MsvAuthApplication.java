e082bc6164bf0773b56000eef6ed7093f27b7a9c
esteban2505J <juane.ramireztqvirtual.edu.co>
2024-09-03 13:44:20 -0500

    feat(mcsv): Microservices added

---
+++ mcsvAuth/src/main/java/com/Auth/MsvAuthApplication.java
@@ -0,0 +1,13 @@
+package com.Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvAuthApplication.class, args);
+	}
+
+}
---
+++ mcsvAuth/src/test/java/com/Auth/MsvAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
