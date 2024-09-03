2f34d722779690916444dbdf5d52ad4a23d457ba
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2024-09-02 22:32:22 -0500

    feat(mcsv): Microservices added
    Added the microservices: Gateway, Eureka and Config

---
+++ microservicio-config/src/test/java/com/microservice/config/MicroservicioConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservicio-gateway/src/main/java/com/microservice/gateway/MicroservicioGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioGatewayApplication.class, args);
+	}
+
+}
