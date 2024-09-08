164a97b18151585f6dcc6f6d90d9bab859a8ae8c
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2024-09-08 18:15:06 -0500

    fix(config): Fixed connection config

--- microservicio-eureka/src/main/java/com/microservice/eureka/MicroservicioEurekaApplication.java
+++ microservicio-eureka/src/main/java/com/microservice/eureka/MicroservicioEurekaApplication.java
@@ -2,6 +2,7 @@ package com.microservice.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
