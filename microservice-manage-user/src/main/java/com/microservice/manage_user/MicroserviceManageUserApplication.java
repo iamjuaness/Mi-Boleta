8887e24d50c7705885102156a77dc3c2c0648fc4
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2024-09-07 22:10:23 -0500

    feat(msvc): Microservice added
    The manage-user microservice was added with its respective configuration, connection with eureka and mongodb

---
+++ microservice-manage-user/src/main/java/com/microservice/manage_user/MicroserviceManageUserApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.manage_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceManageUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceManageUserApplication.class, args);
+	}
+
+}
---
+++ microservice-manage-user/src/test/java/com/microservice/manage_user/MicroserviceManageUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.manage_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceManageUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
