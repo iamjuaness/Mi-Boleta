b5573d18b5b87707945193e2a8f2ec4bf48ff688
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2024-09-22 20:25:45 -0500

    feat(msvc-event): Microservice upgrade
    Se terminaron los metodos basicos del servicio y sus endpoints ademas se agrego la autenticacion de los metodos que no son publicos

--- microservice-manage-event/src/main/java/com/microservice/manage_event/presentation/controller/ManageEventController.java
+++ microservice-manage-event/src/main/java/com/microservice/manage_event/presentation/controller/ManageEventController.java
@@ -1,9 +1,72 @@
 package com.microservice.manage_event.presentation.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microservice.manage_event.persistence.model.enums.State;
+import com.microservice.manage_event.presentation.advice.CustomClientException;
+import com.microservice.manage_event.presentation.dto.CreateEventDTO;
+import com.microservice.manage_event.presentation.dto.UpdateEventDTO;
+import com.microservice.manage_event.presentation.dto.http.MessageDTO;
+import com.microservice.manage_event.service.implementation.EventServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/manage-event")
 public class ManageEventController {
+
+    final EventServiceImpl eventService;
+
+    public ManageEventController(EventServiceImpl eventService) {
+        this.eventService = eventService;
+    }
+
+    /**
+     * This endpoint is used to run the createEvent service
+     * @param createEventDTO event info
+     * @return state action
+     */
+    @PostMapping("/create-event")
+    public ResponseEntity<MessageDTO<State>> createEvent(@RequestBody @Valid CreateEventDTO createEventDTO){
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.createEvent(createEventDTO)));
+        } catch (CustomClientException e){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, null, e.getMessage()));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, null, e.getMessage()));
+        }
+    }
+
+    /**
+     * This endpoint is used to run the updateEvent service
+     * @param updateEventDTO update info event
+     * @param idEvent event's id
+     * @return state action
+     */
+    @PutMapping("/update-event")
+    public ResponseEntity<MessageDTO<State>> updateEvent(@RequestBody @Valid UpdateEventDTO updateEventDTO, @RequestParam String idEvent){
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.updateEvent(updateEventDTO, idEvent)));
+        } catch (CustomClientException e){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, null, e.getMessage()));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, null, e.getMessage()));
+        }
+    }
+
+    /**
+     * This endpoint is used to run the deleteEvent service
+     * @param idEvent event's id
+     * @return state action
+     */
+    @PutMapping("/delete-event")
+    public ResponseEntity<MessageDTO<State>> deleteEvent(@RequestParam String idEvent){
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.deleteEvent(idEvent)));
+        } catch (CustomClientException e){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, null, e.getMessage()));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, null, e.getMessage()));
+        }
+    }
 }
--- microservice-manage-event/src/main/java/com/microservice/manage_event/presentation/controller/ManageEventPublicController.java
+++ microservice-manage-event/src/main/java/com/microservice/manage_event/presentation/controller/ManageEventPublicController.java
@@ -1,9 +1,56 @@
 package com.microservice.manage_event.presentation.controller;
 
+import com.microservice.manage_event.persistence.model.entities.Event;
+import com.microservice.manage_event.presentation.advice.CustomClientException;
+import com.microservice.manage_event.presentation.dto.http.MessageDTO;
+import com.microservice.manage_event.service.implementation.EventServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/public-event")
 public class ManageEventPublicController {
+
+    final EventServiceImpl eventService;
+
+    public ManageEventPublicController(EventServiceImpl eventService) {
+        this.eventService = eventService;
+    }
+
+    /**
+     * This endpoint is used to run the getEvent service
+     * @param idEvent event's id
+     * @return event
+     */
+    @GetMapping("/get-event/{idEvent}")
+    public ResponseEntity<MessageDTO<Event>> getEvent(@PathVariable String idEvent){
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.getEvent(idEvent)));
+        } catch (CustomClientException e){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, null, e.getMessage()));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, null, e.getMessage()));
+        }
+    }
+
+    /**
+     * This endpoint is used to run the getEvents service
+     * @return event list
+     */
+    @GetMapping("/get-events")
+    public ResponseEntity<MessageDTO<List<Event>>> getEvents(){
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.getEvents()));
+        } catch (CustomClientException e){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, null, e.getMessage()));
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, null, e.getMessage()));
+        }
+    }
 }
