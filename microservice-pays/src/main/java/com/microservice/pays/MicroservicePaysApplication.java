4f109e1dc6de0faabae3211fb80791d57554cd83
esteban2505J <juane.ramirezt@uqvirtual.edu.co>
2024-09-28 18:02:42 -0500

    build(msvc-pays): pays microservice created

---
+++ microservice-pays/src/main/java/com/microservice/pays/MicroservicePaysApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.pays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePaysApplication.class, args);
+	}
+
+}
---
+++ microservice-pays/src/test/java/com/microservice/pays/MicroservicePaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.pays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
