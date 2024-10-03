5690b235cfcc62d252e642c600ea06a1be3ded4d
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2024-10-02 20:59:49 -0500

    feat(msvc-event): Endpoints public documented
    Endpoints have been documented event.

--- microservice-manage-event/src/main/java/com/microservice/manage_event/presentation/controller/ManageEventPublicController.java
+++ microservice-manage-event/src/main/java/com/microservice/manage_event/presentation/controller/ManageEventPublicController.java
@@ -6,11 +6,18 @@ import com.microservice.manage_event.presentation.dto.GlobalEventStatsDTO;
 import com.microservice.manage_event.presentation.dto.ListEventStatsDTO;
 import com.microservice.manage_event.presentation.dto.http.MessageDTO;
 import com.microservice.manage_event.service.implementation.EventServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -29,6 +36,66 @@ public class ManageEventPublicController {
      * @return event
      */
     @GetMapping("/get-event/{idEvent}")
+    @Operation(
+            summary = "Get an event by id",
+            description = "Get an event at database by its id",
+            tags = {"Event"},
+            parameters = {
+                    @Parameter(
+                            name = "idEvent",
+                            description = "Event's id",
+                            required = true,
+                            content = @Content(
+                                    mediaType = "String",
+                                    schema = @Schema(
+                                            implementation = String.class
+                                    )
+                            )
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful - Get Event",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Error - Get Event",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request - Invalid input",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - Authentication failed",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    )
+            }
+    )
     public ResponseEntity<MessageDTO<Event>> getEvent(@PathVariable String idEvent){
         try {
             return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.getEvent(idEvent)));
@@ -44,6 +111,66 @@ public class ManageEventPublicController {
      * @return event list
      */
     @GetMapping("/get-events")
+    @Operation(
+            summary = "Get all events",
+            description = "Get all database's events",
+            tags = {"Event"},
+            parameters = {
+                    @Parameter(
+                            name = "idEvent",
+                            description = "Event's id",
+                            required = true,
+                            content = @Content(
+                                    mediaType = "String",
+                                    schema = @Schema(
+                                            implementation = String.class
+                                    )
+                            )
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful - Get Events",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Error - Get Events",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request - Invalid input",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - Authentication failed",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    )
+            }
+    )
     public ResponseEntity<MessageDTO<List<Event>>> getEvents(){
         try {
             return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.getEvents()));
@@ -64,14 +191,118 @@ public class ManageEventPublicController {
      * @return event's list filtered
      */
     @GetMapping("/filter")
-    public List<Event> filterEvents(
+    @Operation(
+            summary = "Filter Events",
+            description = "Filter an event by name, startDate, endDate, address and capacity",
+            tags = {"Event"},
+            parameters = {
+                    @Parameter(
+                            name = "name",
+                            description = "Event's name",
+                            content = @Content(
+                                    mediaType = "String",
+                                    schema = @Schema(
+                                            implementation = String.class
+                                    )
+                            )
+                    ),
+                    @Parameter(
+                            name = "startDate",
+                            description = "Event's startDate",
+                            content = @Content(
+                                    mediaType = "ISOString",
+                                    schema = @Schema(
+                                            implementation = LocalDateTime.class
+                                    )
+                            )
+                    ),
+                    @Parameter(
+                            name = "endDate",
+                            description = "Event's endDate",
+                            content = @Content(
+                                    mediaType = "ISOString",
+                                    schema = @Schema(
+                                            implementation = LocalDateTime.class
+                                    )
+                            )
+                    ),
+                    @Parameter(
+                            name = "address",
+                            description = "Event's address",
+                            content = @Content(
+                                    mediaType = "String",
+                                    schema = @Schema(
+                                            implementation = String.class
+                                    )
+                            )
+                    ),
+                    @Parameter(
+                            name = "capacity",
+                            description = "Event's capacity",
+                            content = @Content(
+                                    mediaType = "Integer",
+                                    schema = @Schema(
+                                            implementation = Integer.class
+                                    )
+                            )
+                    )
+            },
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful - Filter Events",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Error - Filter Events",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request - Invalid input",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - Authentication failed",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<MessageDTO<List<Event>>> filterEvents(
             @RequestParam(required = false) String name,
             @RequestParam(required = false) LocalDateTime startDate,
             @RequestParam(required = false) LocalDateTime endDate,
             @RequestParam(required = false) String address,
             @RequestParam(required = false) Integer capacity) {
-
-        return eventService.filterEvents(name, startDate, endDate, address, capacity != null ? capacity : 0);
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.filterEvents(name, startDate, endDate, address, capacity != null ? capacity : 0)));
+        } catch (CustomClientException e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, Collections.emptyList()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, Collections.emptyList()));
+        }
     }
 
     /**
@@ -79,8 +310,61 @@ public class ManageEventPublicController {
      * @return stats global list
      */
     @GetMapping("/general-stats")
-    public List<GlobalEventStatsDTO> getEventStats() {
-        return eventService.getEventStatistics();
+    @Operation(
+            summary = "General stats Events",
+            description = "General stats events how name, totalTicketsSold, totalCapacity, totalAvailableTickets, totalEvents",
+            tags = {"Stats"},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful - General stats Events",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Error - General stats Events",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request - Invalid input",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - Authentication failed",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<MessageDTO<List<GlobalEventStatsDTO>>> getEventStats() {
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.getEventStatistics()));
+        } catch (CustomClientException e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, Collections.emptyList()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, Collections.emptyList()));
+        }
     }
 
     /**
@@ -88,7 +372,60 @@ public class ManageEventPublicController {
      * @return stats by event list
      */
     @GetMapping("/event-stats")
-    public List<ListEventStatsDTO> getEventStatsByEvent() {
-        return eventService.getStatisticsByEvent();
+    @Operation(
+            summary = "Stats by Events",
+            description = "Stats by event how name, totalTicketsSold, totalCapacity, totalAvailableTickets, totalEvents",
+            tags = {"Stats"},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful - Stats by Event",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "417",
+                            description = "Error - Stats by Event",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = MessageDTO.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad Request - Invalid input",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - Authentication failed",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = ErrorResponse.class
+                                    )
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<MessageDTO<List<ListEventStatsDTO>>> getEventStatsByEvent() {
+        try {
+            return ResponseEntity.ok().body(new MessageDTO<>(false, eventService.getStatisticsByEvent()));
+        } catch (CustomClientException e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new MessageDTO<>(true, Collections.emptyList()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new MessageDTO<>(true, Collections.emptyList()));
+        }
     }
 }
