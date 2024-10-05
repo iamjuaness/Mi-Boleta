b77774a0444dc29899cab6eb3110b8334d8becb0
iamjuaness <juane.cardonav@uqvirtual.edu.co>
2024-10-04 20:23:12 -0500

    feat(msvc-cart): The cart microservice was created
    Se define la configuracion del microservico y se agregan algunas dependecias

---
+++ microservice-cart/src/main/java/com/microservice/cart/MicroserviceCartApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCartApplication.class, args);
+	}
+
+}
